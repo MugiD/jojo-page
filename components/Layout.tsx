1bc08f052161f49df1b1fda8e03df6f58da481b6
Бек Сламбек <slambek_b0607@gmail.com>
2022-08-07 11:38:42 +0600

    add meta tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,10 +8,18 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>JoJo Reference Playlist</title>
+        <meta property="og:image" content="https://i.imgur.com/imagelink.jpg" />
+        <meta property="og:title" content="Website Title" />
+        <meta
+          property="og:description"
+          content="JoJo Reference Playlist is the only playlist you ever need. View some examples of tracks in the Landing Page of this music collection"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://jojo.dosek.xyz/" />
       </Head>
       <Container maxW="container.md" color="white">
         {children}
-      <Footer />
+        <Footer />
       </Container>
     </Box>
   );
